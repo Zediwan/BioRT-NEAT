b67e99eaf1a9a1b97efbf4a131e662764b64e3f4
Zediwan <Jeremy@qualit.ch>
2024-06-15 11:41:49 +0200

    Wrote additional tests for aggregations and activations

--- test/activations_test.py
+++ test/activations_test.py
@@ -6,3 +6,33 @@ class TestGetRandomActivation(unittest.TestCase):
         f = Activations.get_random()
         self.assertIsNotNone(f)
         self.assertTrue(callable(f))
+
+class TestGetOptions(unittest.TestCase):
+    def test_get_options(self):
+        options = Activations.get_options()
+        self.assertIsNotNone(options)
+        self.assertTrue(len(options) > 0)
+        for option in options:
+            self.assertTrue(callable(option))
+
+class TestIsValidActivation(unittest.TestCase):
+    def test_is_valid_activation_exact_activation(self):
+        f = Activations.CLAMPED
+        self.assertTrue(Activations.is_valid_activation(f))
+
+    def test_is_valid_activation_callable(self):
+        f = lambda x: x+1
+        self.assertTrue(Activations.is_valid_activation(f))
+
+    def test_is_valid_activation_not_callable(self):
+        f = 1
+        self.assertFalse(Activations.is_valid_activation(f))
+
+class TestAsssertActivation(unittest.TestCase):
+    def test_assert_activation_valid(self):
+        f = Activations.CLAMPED
+        Activations.assert_activation(f)
+
+    def test_assert_activation_raises(self):
+        f = 1
+        self.assertRaises(ValueError, Activations.assert_activation, f)
--- test/aggregations_test.py
+++ test/aggregations_test.py
@@ -6,3 +6,33 @@ class TestGetRandomActivation(unittest.TestCase):
         f = Aggregations.get_random()
         self.assertIsNotNone(f)
         self.assertTrue(callable(f))
+
+class TestGetOptions(unittest.TestCase):
+    def test_get_options(self):
+        options = Aggregations.get_options()
+        self.assertIsNotNone(options)
+        self.assertTrue(len(options) > 0)
+        for option in options:
+            self.assertTrue(callable(option))
+
+class TestIsValidActivation(unittest.TestCase):
+    def test_is_valid_aggregation_exact_activation(self):
+        f = Aggregations.MAX
+        self.assertTrue(Aggregations.is_valid_aggregation(f))
+
+    def test_is_valid_aggregation_callable(self):
+        f = lambda x: x+1
+        self.assertTrue(Aggregations.is_valid_aggregation(f))
+
+    def test_is_valid_aggregation_not_callable(self):
+        f = 1
+        self.assertFalse(Aggregations.is_valid_aggregation(f))
+
+class TestAsssertActivation(unittest.TestCase):
+    def test_assert_aggregation_valid(self):
+        f = Aggregations.MAX
+        Aggregations.assert_aggregation(f)
+
+    def test_assert_aggregation_raises(self):
+        f = 1
+        self.assertRaises(ValueError, Aggregations.assert_aggregation, f)
