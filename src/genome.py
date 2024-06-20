cf6703a39ac0564b7b17d1ffdbfc6dfc1e085216
Zediwan <Jeremy@qualit.ch>
2024-06-20 12:18:45 +0200

    Implemeted init method for a genome with test

--- src/genome.py
+++ src/genome.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import random
+import itertools
 
 from .config import conf
 from .genes.node import Node
@@ -8,9 +9,72 @@ from .genes.connection import Connection
 
 
 class Genome():
-    def __init__(self, inputs: list[Node], outputs: list[Node], fully_connect: bool = False, num_starting_connections: int | float = None) -> None:
+    def __init__(self, inputs: list[Node] = None, outputs: list[Node] = None, num_inputs: int = None, num_outputs: int = None, fully_connect: bool = None, num_starting_connections: int = None) -> None:
+        if (inputs is None and num_inputs is None) or (inputs is not None and num_inputs is not None):
+            raise ValueError("Cannot have both inputs and num_inputs be None or both defined. (Only one can be given)")
+        if (outputs is None and num_outputs is None) or (outputs is not None and num_outputs is not None):
+            raise ValueError("Cannot have both outputs and num_outputs be None or both defined. (Only one can be given)")
+        if (fully_connect is None and num_starting_connections is None) or (fully_connect is not None and num_starting_connections is not None):
+            raise ValueError("Cannot have both fully_connect and num_starting_connections defined, or cannot have both be undefined. (Only one can be given)")
+
+        self.input_nodes: list[Node] = []
+        self.output_nodes: list[Node] = []
         self.nodes: list[Node] = []
         self.connections: list[Connection] = []
+
+        # Type checks
+        if inputs is not None:
+            if not isinstance(inputs, list) or not all(isinstance(node, Node) for node in inputs):
+                raise TypeError("Inputs must be a list of Node instances.")
+            self.input_nodes = inputs
+            self.nodes.extend(inputs)
+        if outputs is not None:
+            if not isinstance(outputs, list) or not all(isinstance(node, Node) for node in outputs):
+                raise TypeError("Outputs must be a list of Node instances.")
+            self.output_nodes = outputs
+            self.nodes.extend(outputs)
+        if num_inputs is not None:
+            if not isinstance(num_inputs, int):
+                raise TypeError("num_inputs must be an integer.")
+            if num_inputs <= 0:
+                raise ValueError("num_inputs must be a positive integer.")
+            for n in range(num_inputs):
+                input_node = Node()
+                self.input_nodes.append(input_node)
+                self.nodes.append(input_node)
+        if num_outputs is not None:
+            if not isinstance(num_outputs, int):
+                raise TypeError("num_outputs must be an integer.")
+            if  num_outputs <= 0:
+                raise ValueError("num_outputs must be a positive integer.")
+            for n in range(num_outputs):
+                output_node = Node()
+                self.output_nodes.append(output_node)
+                self.nodes.append(output_node)
+        if fully_connect is not None:
+            if not isinstance(fully_connect, bool):
+                raise TypeError("fully_connect must be a boolean.")
+        if num_starting_connections is not None:
+            if not isinstance(num_starting_connections, int):
+                raise TypeError("num_starting_connections must be an integer.")
+            if num_starting_connections <= 0:
+                raise ValueError("num_starting_connections must be a positive integer.")
+            # Num connections cannot exceed the maximum possible connections
+            num_starting_connections = max(num_starting_connections, len(self.input_nodes) * len(self.output_nodes))
+            # Randomly choose connections to be made
+            cons: list[Connection] = random.choices(
+                list[itertools.product(self.input_nodes, self.output_nodes)],
+                k = num_starting_connections
+            )
+        elif not fully_connect:
+            raise ValueError("Cannot have fully_connect be false and not provide num_starting_connections")
+        else:
+            self._fully_connect()
+
+    def _fully_connect(self):
+        for input_node in self.input_nodes:
+            for output_node in self.output_nodes:
+                self._add_connection(input_node, output_node)
 
     def mutate(self) -> None:
         pass
@@ -28,7 +92,9 @@ class Genome():
         pass
     
     def _add_connection(self, from_node: Node, to_node: Node) -> None:
-        pass
+        # TODO typecheck
+        # TODO check if connection already exists between the two
+        self.connections.append(Connection(from_node=from_node, to_node=to_node))
     
     def _add_node(self, connection: Connection) -> None:
         pass
@@ -42,6 +108,9 @@ class Genome():
     def copy(self) -> Genome:
         pass
     
+    def are_nodes_connected(self, n1: Node, n2: Node) -> bool:
+        pass
+
     @staticmethod
     def crossover(g1: Genome, g2: Genome) -> Genome:
         pass
--- test/genome_test.py
+++ test/genome_test.py
@@ -9,23 +9,252 @@ from src.config import conf
 
 class TestGenome(unittest.TestCase):
     def setUp(self) -> None:
+        self.input_node_1 = Node()
+        self.input_node_2 = Node()
+        self.input_nodes = [self.input_node_1, self.input_node_2]
+        self.num_inputs = len(self.input_nodes)
+
+        self.output_node_1 = Node()
+        self.output_nodes = [self.output_node_1]
+        self.num_outputs = len(self.output_nodes)
+
+        self.new_node_proba = conf.mut.struct.new_node_proba
+        self.new_conn_proba = conf.mut.struct.new_conn_proba
+        self.del_node_proba = conf.mut.struct.del_node_proba
+        self.del_conn_proba = conf.mut.struct.del_conn_proba
+        self.max_hidden_nodes = conf.mut.struct.max_hidden_nodes
+
+    def tearDown(self) -> None:
+        conf.mut.struct.new_node_proba = self.new_node_proba
+        conf.mut.struct.new_conn_proba = self.new_conn_proba
+        conf.mut.struct.del_node_proba = self.del_node_proba
+        conf.mut.struct.del_conn_proba = self.del_conn_proba
+        conf.mut.struct.max_hidden_nodes = self.max_hidden_nodes
+
+class TestInit_OutputsInputs_FullyConnected(TestGenome):
+    def setUp(self) -> None:
+        super().setUp()
+        self.genome = Genome(inputs=self.input_nodes, outputs=self.output_nodes, fully_connect=True)
+
+    def test_input_nodes_matching(self):
+        # Check if the input nodes given are actually in the genome
+        for input_node in self.input_nodes:
+            self.assertIn(input_node, self.genome.input_nodes, "Input node not in genomes input nodes")
+            self.assertIn(input_node, self.genome.nodes, "Input node not in genomes nodes")
+        # Check that there are no output nodes in the genome that were not given
+        for input_node in self.genome.input_nodes:
+            self.assertIn(input_node, self.input_nodes, "Genome input node found that was not expected")
+
+    def test_output_nodes_matching(self):
+        # Check if the output nodes given are actually in the genome
+        for output_node in self.output_nodes:
+            self.assertIn(output_node, self.genome.output_nodes, "Output node not in genomes output nodes")
+            self.assertIn(output_node, self.genome.nodes, "Output node not in genomes nodes")
+        # Check that there are no output nodes in the genome that weren't given
+        for output_node in self.genome.output_nodes:
+            self.assertIn(output_node, self.output_nodes, "Genome output node found that was not expected")
+
+    def test_num_nodes(self):
+        self.assertEqual(self.num_inputs + self.num_outputs, len(self.genome.nodes), "Number of nodes not as expected")
+        self.assertEqual(self.num_inputs, len(self.genome.input_nodes), "Number of inputs not as expected")
+        self.assertEqual(self.num_outputs, len(self.genome.output_nodes), "Number of outputs not as expected.")
+
+    def test_connections(self):
+        # Check connections
+        for input_node in self.genome.input_nodes:
+            # Check the input node does not have any input connections
+            self.assertEqual(input_node.num_in_connections, 0, "Input node has input connections.")
+            # Check that the input node has the expected amount of output connections
+            self.assertEqual(len(input_node.out_connections), self.num_outputs, "Input node has more connections than expected.")
+            # Go over all the output connections the input node has
+            for out_connection in input_node.out_connections:
+                # Check that each connection of the input node is towards an output node
+                self.assertIn(out_connection.TO_NODE, self.genome.output_nodes, "Input node has a connection to a non-output node.")
+            for output_node in self.genome.output_nodes:
+                # Check that each output node is connected to the input
+                self.assertIn(output_node, [conn.TO_NODE for conn in input_node.out_connections], "Output node is not connected to input.")
+
+        for output_node in self.genome.output_nodes:
+            # Check that the number of input connections is equal to the number of inputs (as the genome is fully connected)
+            self.assertEqual(output_node.num_in_connections, self.num_inputs, "Output nodes number of connections does not match the expected amount.")
+            # Check that the output does not have any output connections
+            self.assertEqual(len(output_node.out_connections), 0, "Output node has output connections")
+
+class TestInit_NumOutputsNumInputs_FullyConnected(TestGenome):
+    def setUp(self) -> None:
+        super().setUp()
+        self.genome = Genome(num_inputs=self.num_inputs, num_outputs=self.num_outputs, fully_connect=True)
+
+    def test_num_nodes(self):
+        self.assertEqual(self.num_inputs + self.num_outputs, len(self.genome.nodes), "Number of nodes not as expected")
+        self.assertEqual(self.num_inputs, len(self.genome.input_nodes), "Number of inputs not as expected")
+        self.assertEqual(self.num_outputs, len(self.genome.output_nodes), "Number of outputs not as expected.")
+
+    def test_connections(self):
+        # Check connections
+        for input_node in self.genome.input_nodes:
+            # Check the input node does not have any input connections
+            self.assertEqual(input_node.num_in_connections, 0, "Input node has input connections.")
+            # Check that the input node has the expected amount of output connections
+            self.assertEqual(len(input_node.out_connections), self.num_outputs, "Input nodes number of connections does not match the expected amount.")
+            # Go over all the output connections the input node has
+            for out_connection in input_node.out_connections:
+                # Check that each connection of the input node is towards an output node
+                self.assertIn(out_connection.TO_NODE, self.genome.output_nodes, "Input node has a connection to a non-output node.")
+            for output_node in self.genome.output_nodes:
+                # Check that each output node is connected to the input
+                self.assertIn(output_node, [conn.TO_NODE for conn in input_node.out_connections], "Output node is not connected to input.")
+
+        for output_node in self.genome.output_nodes:
+            # Check that the number of input connections is equal to the number of inputs (as the genome is fully connected)
+            self.assertEqual(output_node.num_in_connections, self.num_inputs, "Output nodes number of connections does not match the expected amount.")
+            # Check that the output does not have any output connections
+            self.assertEqual(len(output_node.out_connections), 0, "Output node has output connections")
+
+class TestInit_OutputsInputs_NumConnections(TestGenome):
+    pass
+
+    def test_init_num_connections_exeeds_max(self):
         pass
 
-class TestInit(TestGenome):
-    def test_init_defaults(self):
-        pass
+class TestInit_NumOutputsNumInputs_NumConnections(TestGenome):
+    pass
+
+class TestInit_Exceptions(TestGenome):
+    def test_init_both_inputs_outputs_and_num_inputs_num_outputs_defined_exception(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            inputs=self.input_nodes,
+            outputs=self.output_nodes,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            fully_connect=True
+        )
+
+    def test_init_neither_inputs_outputs_and_num_inputs_num_outputs_defined_exception(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            fully_connect=True
+        )
+
+    def test_init_invalid_zero_num_inputs(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=0,
+            num_outputs=self.num_outputs,
+            fully_connect=True
+        )
     
-    def test_init_valid_args(self):
-        pass
+    def test_init_invalid_negative_num_inputs(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=-1,
+            num_outputs=self.num_outputs,
+            fully_connect=True
+        )
     
-    def test_init_fully_connect(self):
-        pass
+    def test_init_invalid_zero_num_outputs(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=0,
+            fully_connect=True
+        )
     
-    def test_init_num_connections(self):
-        pass
+    def test_init_invalid_negative_num_outputs(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=-1,
+            fully_connect=True
+        )
+
+    def test_init_both_fully_connect_num_connections(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            fully_connect=True,
+            num_starting_connections= (self.num_inputs * self.num_outputs)
+        )
+
+    def test_init_fully_connect_false_num_connections_none(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            fully_connect=False,
+        )
+
+    def test_init_invalid_zero_num_connections(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            num_starting_connections=0
+        )
     
+    def test_init_invalid_zero_num_connections(self):
+        self.assertRaises(
+            ValueError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            num_starting_connections=-1
+        )
+
     def test_init_invalid_type_args(self):
-        pass
+        self.assertRaises(
+            TypeError,
+            Genome,
+            inputs="inputs",
+            outputs=self.output_nodes,
+            fully_connect=True
+        )
+        self.assertRaises(
+            TypeError,
+            Genome,
+            inputs=self.input_nodes,
+            outputs="outputs",
+            fully_connect=True
+        )
+        self.assertRaises(
+            TypeError,
+            Genome,
+            inputs=self.input_nodes,
+            outputs=self.output_nodes,
+            fully_connect="True"
+        )
+        self.assertRaises(
+            TypeError,
+            Genome,
+            num_inputs="num_inputs",
+            num_outputs=self.num_outputs,
+            fully_connect=True
+        )
+        self.assertRaises(
+            TypeError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs="num_outputs",
+            fully_connect=True
+        )
+        self.assertRaises(
+            TypeError,
+            Genome,
+            num_inputs=self.num_inputs,
+            num_outputs=self.num_outputs,
+            num_starting_connections = "1"
+        )
 
 class TestMutate(TestGenome):
     pass
@@ -55,6 +284,9 @@ class TestDeleteNode(TestGenome):
     pass
 
 class TestCopy(TestGenome):
+    pass
+
+class TestAreNodesConnected(TestGenome):
     pass
 
 class TestCrossover(TestGenome):
